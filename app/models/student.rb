2d13049acfe30cee42552fa35d27225182be10f1
J Connolly <jsconnolly1@gmail.com>
2013-03-07 11:27:57 -0800

    start implementing student model including initialize, set_attributes and name methods

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,23 @@
 require_relative '../../db/config'
 
-# implement your Student model here
+class Student
+  def initialize(*args)
+    @first_name = args[:first_name]
+    @last_name = args[:last_name]
+    @gender = args[:gender]
+    @birthday = args[:birthday]
+  end
+
+  def assign_attributes(args)
+    @first_name = args[:first_name] || @first_name
+    @last_name = args[:last_name] || @last_name
+    @gender = args[:gender] || @gender
+    @birthday = args[:birthday] || @birthday
+
+  end
+
+  def name
+    "#{@first_name} #{@last_name}"
+  end
+  
+end
