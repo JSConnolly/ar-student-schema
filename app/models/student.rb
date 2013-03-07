126e7be9442a29ad0afe16aeeed35f5469497f54
J Connolly <jsconnolly1@gmail.com>
2013-03-07 15:58:43 -0800

    all validation tests pass

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,24 +1,28 @@
 require_relative '../../db/config'
 
+
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
+class PhoneValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /^\(?\d{3}\)?(-|\.|\s)?\d{3}(\)|-|\.|\s)?\d{4}(\)|-|\.|\s)?\s?(x\s?\d+)?$/i
+      record.errors[attribute] << (options[:message] || "is not a valid phone")
+    end
+  end
+end
+
+
+
 class Student < ActiveRecord::Base
-  # def initialize(args = {})
-  #   @first_name = args[:first_name]
-  #   @last_name = args[:last_name]
-  #   @gender = args[:gender]
-  #   @birthday = args[:birthday]
-  # end
-
-  # has_one :first_name
-  # has_one :last_name
-  # has_one :gender
-
-  # def assign_attributes(args)
-  #   @first_name = args[:first_name] || @first_name
-  #   @last_name = args[:last_name] || @last_name
-  #   @gender = args[:gender] || @gender
-  #   @birthday = args[:birthday] || @birthday
-
-  # end
+  validates :email, :email => true, :uniqueness => true
+  validates :phone, :phone => true
+  validate :age_greater_than_5
 
   def name
     "#{first_name} #{last_name}"
@@ -31,20 +35,15 @@ class Student < ActiveRecord::Base
     (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
   end
 
+  def age_greater_than_5
+    if age < 5
+      errors.add(:age, "can't be less than 5")
+    end
+  end
+
   def self.delete_all
     Student.destroy_all
   end
 
 end
 
-# Student.delete_all
-# student = Student.new
-# student.assign_attributes(
-#                           :first_name => "Happy",
-#                           :last_name => "Gilmore",
-#                           :gender => 'male',
-#                           :birthday => Date.new(1970,9,1)
-#     )
-# student.save
-
-# puts student.name
