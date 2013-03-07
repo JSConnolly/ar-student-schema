7a934b1a061bff18fe5489718fa6c1be1aced23f
J Connolly <jsconnolly1@gmail.com>
2013-03-07 15:23:35 -0800

    passing student model tests

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,30 +1,43 @@
 require_relative '../../db/config'
 
-class Student
-  def initialize(args = {})
-    @first_name = args[:first_name]
-    @last_name = args[:last_name]
-    @gender = args[:gender]
-    @birthday = args[:birthday]
-  end
+class Student < ActiveRecord::Base
+  # def initialize(args = {})
+  #   @first_name = args[:first_name]
+  #   @last_name = args[:last_name]
+  #   @gender = args[:gender]
+  #   @birthday = args[:birthday]
+  # end
 
-  def assign_attributes(args)
-    @first_name = args[:first_name] || @first_name
-    @last_name = args[:last_name] || @last_name
-    @gender = args[:gender] || @gender
-    @birthday = args[:birthday] || @birthday
+  # has_one :first_name
+  # has_one :last_name
+  # has_one :gender
 
-  end
+  # def assign_attributes(args)
+  #   @first_name = args[:first_name] || @first_name
+  #   @last_name = args[:last_name] || @last_name
+  #   @gender = args[:gender] || @gender
+  #   @birthday = args[:birthday] || @birthday
+
+  # end
 
   def name
-    "#{@first_name} #{@last_name}"
+    "#{first_name} #{last_name}"
   end
 
-  def delete_students
+  def age
+    now = Date.today
+    
+    now.year - birthday.year - ((now.month > birthday.month ||
+    (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+  end
+
+  def self.delete_all
+    Student.destroy_all
   end
 
 end
 
+# Student.delete_all
 # student = Student.new
 # student.assign_attributes(
 #                           :first_name => "Happy",
@@ -32,5 +45,6 @@ end
 #                           :gender => 'male',
 #                           :birthday => Date.new(1970,9,1)
 #     )
+# student.save
 
 # puts student.name
