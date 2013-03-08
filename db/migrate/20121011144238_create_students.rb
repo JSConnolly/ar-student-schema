509d6c9bcb1f96b952322a2e9667029302964ae4
J Connolly <jsconnolly1@gmail.com>
2013-03-07 16:14:36 -0800

    final cleanup

--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -1,12 +1,8 @@
 require_relative '../config'
 
-puts 'running migrate script...'
-# this is where you should use an ActiveRecord migration to 
-
 class CreateStudents < ActiveRecord::Migration
   def up
     puts "adding table"
-    # HINT: checkout ActiveRecord::Migration.create_table
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
